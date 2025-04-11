1bd432589c3c71c6a3c814cf77a4a798607fca22
Rakesh Banda <rakeshbanda@Rakeshs-MacBook-Air.local>
2025-04-11 13:49:14 -0700

    edited

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ console.log('Portfolio loaded!');
 document.getElementById('darkModeToggle').addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
 });
+const toggle = document.getElementById('darkModeToggle');
+const label = document.getElementById('modeLabel');
+
+// Click sound effect
+const clickSound = new Audio("https://www.soundjay.com/buttons/sounds/button-16.mp3");
+
+toggle.addEventListener('change', function () {
+  document.body.classList.toggle('dark-mode');
+  clickSound.play();
+  
+  if (this.checked) {
+    label.textContent = "Dark Mode: On";
+  } else {
+    label.textContent = "Dark Mode: Off";
+  }
+});
+
